21f0f22394f90daa74ef91ab06f351495378a0e0
syedBahadarKhan <ba4646465@gmail.com>
2025-02-16 14:17:08 +0500

    project section and servise section styling has been completed

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 
 const hamburger = document.querySelector(".hamburger");
 const mobileMenu = document.querySelector(".nav-list ul")
+const navMainu = document.querySelectorAll(".nav-list ul li a")
+const header = document.querySelector(".header");
+
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     mobileMenu.classList.toggle("active");
 });
 
+
+
+navMainu.forEach((item) =>{
+    item.addEventListener("click", () =>{
+        hamburger.classList.toggle("active");
+        mobileMenu.classList.toggle("active");
+    });
+});
+
+
+document.addEventListener("scroll", ()=>{
+    let scroll_Position = window.scrollY;
+    if(scroll_Position > 200){
+        header.style.backgroundColor = "#29323c";
+    }else{
+        header.style.backgroundColor ="transparent";
+    }
+});
